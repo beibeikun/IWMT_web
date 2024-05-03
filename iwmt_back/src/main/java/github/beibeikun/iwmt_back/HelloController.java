6ad7f53de5ac1c92635bffc3ece88a8ce915cf81
beibeikun <cjzhelloworld@outlook.com>
2024-05-03 23:06:00 +0900

    -

--- iwmt_back/src/main/java/github/beibeikun/iwmt_back/HelloController.java
+++ iwmt_back/src/main/java/github/beibeikun/iwmt_back/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @CrossOrigin(origins = "http://localhost:5173") // 替换为你的Vue应用的实际URL和端口
+    @CrossOrigin(origins = "http://localhost:3000") // 替换为你的Vue应用的实际URL和端口
     @GetMapping("/api/hello")
     public String hello() {
         return "{\"message\":\"Hello, Spring Boot from Vue!\"}";
